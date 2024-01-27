07a6211ca77959e38997892f23cfb1fde8120b0f
Chad <chadjcampbell@gmail.com>
2024-01-26 20:42:51 -0500

    remove context, update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>Home</h1>
-      </div>
+    <main className="min-h-dvh w-dvw bg-black">
+      <nav className="flex w-full items-center justify-between ">
+        <Link href="/" className="m-6">
+          <Image
+            src={"/logos/logo-no-background.png"}
+            alt="Flix with Friends Logo"
+            width={200}
+            height={30}
+          />
+        </Link>
+        <div className="mr-6 border-solid border-4 border-purple-800 rounded-full">
+          <UserButton
+            appearance={{
+              elements: {
+                userButtonAvatarBox: "h-14 w-auto",
+              },
+            }}
+            afterSignOutUrl="/landing"
+          />
+        </div>
+      </nav>
     </main>
   );
 }
--- src/context/index.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React, { createContext } from "react";
-
-export const GlobalContext = createContext<null | {}>(null);
-
-export const GlobalState = ({ children }: { children: React.ReactNode }) => {
-  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
-};
