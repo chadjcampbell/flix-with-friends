dbe23338361d18dbbeb3ca33340d9433b7102fef
Chad <chadjcampbell@gmail.com>
2024-02-02 20:25:09 -0500

    update next config for oslo

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
+    return config;
+  },
+};
+module.exports = nextConfig;
