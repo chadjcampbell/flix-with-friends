5ef596cd9931ff1caf6ab8a20a1c9b605030358f
Chad <chadjcampbell@gmail.com>
2024-02-07 19:30:17 -0500

    add nextui styles to auth forms

--- src/app/no-auth/signin/SignInForm.tsx
+++ src/app/no-auth/signin/SignInForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import signin from "@/actions/auth/signin";
+import { Button, Card, Input } from "@nextui-org/react";
 import Link from "next/link";
 import { useFormState } from "react-dom";
 
@@ -8,50 +9,48 @@ const SignInForm = () => {
   const initialState = { error: null };
   const [state, dispatch] = useFormState(signin, initialState);
   return (
-    <form
-      className="bg-white p-6 flex flex-col gap-4 rounded-md"
-      action={dispatch}
-    >
-      <label htmlFor="username">Username</label>
-      <input
-        autoComplete="username"
-        className="border-solid border-2 border-black p-2 rounded-md"
-        name="username"
-        id="username"
-        required
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        autoComplete="current-password"
-        className="border-solid border-2 border-black p-2 rounded-md"
-        type="password"
-        name="password"
-        id="password"
-        required
-      />
-      <br />
-      <div id="form-error" aria-live="polite" aria-atomic="true">
-        {state.error ? (
-          <p className="mt-2 text-sm text-red-500" key={state.error}>
-            {state.error}
-          </p>
-        ) : (
-          <br />
-        )}
-      </div>
-      <button
-        type="submit"
-        className="bg-indigo-800 text-white text-xl px-6 py-2 rounded-lg hover:bg-indigo-600"
-      >
-        Sign In
-      </button>
-      <p className="text-center">
-        Not yet a member?{" "}
-        <Link className="text-red-800" href="/no-auth/signup">
-          Sign Up
-        </Link>
-      </p>
-    </form>
+    <Card>
+      <form className="p-6 flex flex-col gap-4" action={dispatch}>
+        <label htmlFor="username">Username</label>
+        <Input
+          type="username"
+          autoComplete="username"
+          name="username"
+          id="username"
+          isRequired
+        />
+        <label htmlFor="password">Password</label>
+        <Input
+          autoComplete="current-password"
+          type="password"
+          name="password"
+          id="password"
+          isRequired
+        />
+        <br />
+        <div id="form-error" aria-live="polite" aria-atomic="true">
+          {state.error ? (
+            <p className="mt-2 text-sm text-red-500" key={state.error}>
+              {state.error}
+            </p>
+          ) : (
+            <br />
+          )}
+        </div>
+        <Button
+          type="submit"
+          className="bg-purple-800 text-white text-xl px-6 py-2 rounded-lg hover:bg-indigo-600"
+        >
+          Sign In
+        </Button>
+        <p className="text-center">
+          Not yet a member?{" "}
+          <Link className="text-red-800" href="/no-auth/signup">
+            Sign Up
+          </Link>
+        </p>
+      </form>
+    </Card>
   );
 };
 
--- src/app/no-auth/signup/SignUpForm.tsx
+++ src/app/no-auth/signup/SignUpForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import signup from "@/actions/auth/signup";
+import { Button, Card, Input } from "@nextui-org/react";
 import Link from "next/link";
 import { useFormState } from "react-dom";
 
@@ -8,56 +9,53 @@ const SignUpForm = () => {
   const initialState = { error: null };
   const [state, dispatch] = useFormState(signup, initialState);
   return (
-    <form
-      className="bg-white p-6 flex flex-col gap-4 rounded-md"
-      action={dispatch}
-    >
-      <label htmlFor="username">Username</label>
-      <input
-        autoComplete="username"
-        required
-        className="border-solid border-2 border-black p-2 rounded-md"
-        name="username"
-        id="username"
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        autoComplete="new-password"
-        required
-        className="border-solid border-2 border-black p-2 rounded-md"
-        type="password"
-        name="password"
-        id="password"
-      />
-      <label htmlFor="confirmPassword">Confirm Password</label>
-      <input
-        autoComplete="new-password"
-        required
-        className="border-solid border-2 border-black p-2 rounded-md"
-        type="password"
-        name="confirmPassword"
-        id="confirmPassword"
-      />
-      {state.error ? (
-        <p className="mt-2 text-sm text-red-500" key={state.error}>
-          {state.error}
+    <Card>
+      <form className="p-6 flex flex-col gap-4" action={dispatch}>
+        <label htmlFor="username">Username</label>
+        <Input
+          type="username"
+          autoComplete="username"
+          isRequired
+          name="username"
+          id="username"
+        />
+        <label htmlFor="password">Password</label>
+        <Input
+          autoComplete="new-password"
+          isRequired
+          type="password"
+          name="password"
+          id="password"
+        />
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <Input
+          autoComplete="new-password"
+          isRequired
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+        />
+        {state.error ? (
+          <p className="mt-2 text-sm text-red-500" key={state.error}>
+            {state.error}
+          </p>
+        ) : (
+          <br />
+        )}
+        <Button
+          type="submit"
+          className="bg-red-800 text-white text-xl px-6 py-2 rounded-lg hover:bg-red-600"
+        >
+          Sign Up
+        </Button>
+        <p className="text-center">
+          Already a member?{" "}
+          <Link className="text-indigo-800" href="/no-auth/signin">
+            Sign In
+          </Link>
         </p>
-      ) : (
-        <br />
-      )}
-      <button
-        type="submit"
-        className="bg-red-800 text-white text-xl px-6 py-2 rounded-lg hover:bg-red-600"
-      >
-        Sign Up
-      </button>
-      <p className="text-center">
-        Already a member?{" "}
-        <Link className="text-indigo-800" href="/no-auth/signin">
-          Sign In
-        </Link>
-      </p>
-    </form>
+      </form>
+    </Card>
   );
 };
 
